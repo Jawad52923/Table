1cb2a9783fe78c327e6b849627c1634fd8172fc5
Your Name <you@example.com>
2020-12-21 20:59:33 +0500

    first commit

---
+++ table program/table program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace table_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Write the table num");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(num +"x"+ i +"="+ i*num);
+               // Console.WriteLine("{0}*{1}={2}", num,i,i*num );
+          
+            }
+        }
+    }
+}
---
+++ table-using-while-loop/table-using-while-loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace table_using_while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            Console.WriteLine("Enter a number");
+           int num1 = Convert.ToInt32(Console.ReadLine());
+
+          int  i = 1; //Initialization
+            //Check whether condition matches or not
+            do
+            {
+
+               
+                Console.WriteLine("{0} x {1} = {2}", num1, i,  num1 * i);
+                i++; //Increment by one
+            } while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
